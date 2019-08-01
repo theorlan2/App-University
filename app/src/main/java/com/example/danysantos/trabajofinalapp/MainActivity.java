627998231e795c171785f9c7358e6404fd8ba24f
Dany <theorlan2@gmail.com>
2019-07-31 22:02:36 -0400

    first commit...

---
+++ app/src/main/java/com/example/danysantos/trabajofinalapp/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.danysantos.trabajofinalapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+}
