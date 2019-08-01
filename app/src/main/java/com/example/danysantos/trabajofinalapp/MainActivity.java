54823de24cb1e112633b556aa495027abde80c5d
Dany <theorlan2@gmail.com>
2019-08-01 08:04:29 -0400

    Nueva vista y arreglos de la aplicacion...

--- app/src/main/java/com/example/danysantos/trabajofinalapp/MainActivity.java
+++ app/src/main/java/com/example/danysantos/trabajofinalapp/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.danysantos.trabajofinalapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,7 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button MyButton = (Button) findViewById(R.id.button01);
 
+        MyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ListSong = new Intent(getApplicationContext(), otherActivity.class);
+                startActivity(ListSong);
+            }
+        });
+
+    }
+
+
+    public  void Controller() {
 
     }
 
---
+++ app/src/main/java/com/example/danysantos/trabajofinalapp/otherActivity.java
@@ -0,0 +1,13 @@
+package com.example.danysantos.trabajofinalapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class otherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_other);
+    }
+}
